4be1239d22cc569a39aeb2ad9f857984695ba80d
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-11-21 13:58:24 +0200

    Clean up

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import src.outcar_data_extracter as ode
+import json
+
+def get_data_test():
+    data = ode.processe_data("./data/New_opt_p3x3", True)
+    with open("./data/test_out.json", 'w') as file:
+        file.write(json.dumps(data, indent=2))
 
 def main():
-    ...
+    get_data_test()
 
 if __name__ == "__main__": main()
--- src/outcar_data_extracter.py
+++ src/outcar_data_extracter.py
@@ -1,6 +1,4 @@
-import glob
-import os
-import json
+import glob, os
 
 def find_file(file_name:str, folder:str) -> list:
     result = []
@@ -11,45 +9,32 @@ def find_file(file_name:str, folder:str) -> list:
     
     return result
 
-def get_data(folder:str) -> list:
+def get_data(folder:str) -> dict:
+    '''
+    Gets the data from a folder
+    Args:
+        folder (str): Path of the folder
+    '''
     try:
-        # energy = None
         # OUTCAR FILE
         with open(fr"{folder}\OUTCAR", 'r') as outcar_file:
             outcar = outcar_file.readlines()
         index = 1
-        done_energy = False
-        done_forces = False
         for line in outcar[::-1]:
             index += 1
-            if "energy(sigma->0) =" in line.strip():      
-                # print(len(outcar) - index)
-        
+            if "energy(sigma->0) =" in line.strip():              
                 break
         
         energy = {}
         line_number = len(outcar) - index
 
-        # # Energy Toten
-        # line = outcar[line_number+3].split()
-        # # print(line)
-        # energy["toten"] = line[4]
-
-        # # Energy without entropy & energy sigma->0
+        # sigma->0
         line = outcar[line_number+1].split()
-        # print(line_number)
-        # print(line)
-        # # print(line)
-        # energy["without_entropy"] = line[3]
         try:
             line.remove("=")
             line.remove("=")
         except: pass
-        # print(line)
-        energy["sigma_0"] = line[5]
-        # print(energy["sigma_0"])
-
-        # print(energy)        
+        energy["sigma_0"] = line[5]     
 
     except FileNotFoundError:
         raise Exception(f"There was no OUTCAR file found in {folder}")
@@ -59,17 +44,11 @@ def get_data(folder:str) -> list:
     elements = []
     poscar_file_names = find_file("POSCAR", folder)
     for poscar_file_name in poscar_file_names:
-        # print(poscar_file)
         try:       
             with open(fr'{folder}\{poscar_file_name}', 'r') as poscar_file:
                 poscar = poscar_file.readlines()
             
-              # elements[poscar_file_name.replace('POSCAR_', '')] = ({k:j for k, j in zip(poscar[5].split(), poscar[6].split())})
             elements += poscar[5].split()
-
-
-            
-            # print(elements, "\n\n")
         
         except FileNotFoundError:
             raise Exception(f"Could not find file {folder}\\{poscar_file}")
@@ -77,37 +56,32 @@ def get_data(folder:str) -> list:
         else:
             elements = list(dict.fromkeys(elements))
 
+    # use the folder name to find which Pt is being used
     if "Pt" in elements:
         elements.remove("Pt")
 
-        elements.insert(0, folder.split('\\')[::-1][0].split("_")[0])
-
+        elements.insert(0, folder.split('\\')[::-1][0].split("_")[0])           
     
-    print(f"{elements}\n")
-
-            
-    
-    # return elements, energy["sigma_0"]
     return {"elements":elements, "energy": energy["sigma_0"], "file": folder}
 
 
 
-def processe_data(folder:str, recursive=False):
+def processe_data(folder:str, recursive=False) -> list[dict]:
+    '''
+    Takes the folder where the vasprun.xml is and extracts the data
+
+    Args:
+        folder (str): the path to the folder
+        recursive (bool): Look through all folders for all folders with vasprun.xml
+
+    Returns:
+        list[dict]: A list of dictionaries with all the data
+    '''
     if not recursive:
-        return get_data(folder=folder)
+        return [get_data(folder=folder)]
     else:
         out = []
         for filename in glob.glob(f'{folder}\\**\\vasprun.xml', recursive=True):
             out.append(get_data(filename.replace("\\vasprun.xml", "")))
         
         return out
-
-count_total = 0
-count = 0
-l = processe_data('./data/New_opt_p3x3', recursive=True)
-
-with open("./data/test_out.json", 'w') as file:
-    file.write(json.dumps(l, indent=2))
-
-# p = get_data('./data/New_opt_p3x3\\Coverage_lat_int_OH_Pt_p3x3_copy\\Pt111_2xOH\\Pt111_2xOH_vasp')
-# print(p)
