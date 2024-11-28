0ec46d187c5661944681eafba0176673090f8c80
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-11-28 12:02:37 +0200

    Added the logger

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -2,7 +2,8 @@ from .classes import Force, Position, Atom
 from .files import file_not_found, find_file_like, scandir
 from . import exceptions
 from . import distance
+from .logger import logger
 
 __all__ = ["Force", "Position", "file_not_found",
            "find_file_like", "scandir", "exceptions", "distance",
-           "Atom"]
+           "Atom", "logger"]
--- src/util/logger.py
+++ src/util/logger.py
@@ -1,4 +1,30 @@
+import datetime
+
 
 class Logger:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, save_path: str = "./logs.log") -> None:
+        self.save_path = save_path
+
+    def info(self, unit: str, msg: str):
+        now = datetime.datetime.now()
+        line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/INFO]:\t{msg}" # noqa
+        with open(self.save_path, "a") as file:
+            file.write(line + "\n")
+        print(line)
+
+    def warn(self, unit: str, msg: str):
+        now = datetime.datetime.now().time()
+        line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/WARN]:\t{msg}" # noqa
+        with open(self.save_path, "a") as file:
+            file.write(line + "\n")
+        print(line)
+    
+    def error(self, unit: str, msg: str):
+        now = datetime.datetime.now().time()
+        line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/ERROR]:\t{msg}" # noqa
+        with open(self.save_path, "a") as file:
+            file.write(line + "\n")
+        print(line)
+
+
+logger = Logger()
