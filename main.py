d424ebe5ad3a20c74a5635549e348bf003b7b476
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-12-02 07:46:13 +0200

    Logging

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from src.response_handler import ResponseHandler
 from pathlib import Path
+from src.util import logger
+
+
+def create_folders():
+    folders_needed = [
+        "./data/",
+        "./data/chgnet",
+        "./data/chgnet/models",
+        "./data/chgnet/json",
+        "./data/OUTCAR"
+    ]
+    logger.info("File Checker", "Checking if Folders exists",
+                False)
+
+    for folder in folders_needed:
+        if not Path(folder).is_dir():
+            logger.warn("File Checker", f"folder '{folder}' does not exists",
+                        False)
+            Path(folder).mkdir(parents=True, exist_ok=True)
+            logger.info("File Checker", f"folder '{folder}' was created",
+                        False)
 
 
 def main():
-    Path("./data/").mkdir(parents=True, exist_ok=True)
+    logger.info("Start", "Starting Setup",
+                False)
+    create_folders()
+    from src.response_handler import ResponseHandler
     running = True
     handler = ResponseHandler()
+    logger.info("Start", "Starting",
+                False)
     while running:
         command = input("Enter command> ").lower()
 
         if command in ["q", "quit", "close", 'stop']:
+            logger.info("Program", "Stopping")
             running = False
         else:
             handler.handler(command)
--- src/util/logger.py
+++ src/util/logger.py
@@ -5,26 +5,29 @@ class Logger:
     def __init__(self, save_path: str = "./logs.log") -> None:
         self.save_path = save_path
 
-    def info(self, unit: str, msg: str):
+    def info(self, unit: str, msg: str, silent: bool = True):
         now = datetime.datetime.now()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/INFO]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
             file.write(line + "\n")
-        print(line)
+        if not silent:
+            print(line)
 
-    def warn(self, unit: str, msg: str):
+    def warn(self, unit: str, msg: str, silent: bool = True):
         now = datetime.datetime.now().time()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/WARN]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
             file.write(line + "\n")
-        print(line)
-    
-    def error(self, unit: str, msg: str):
+        if not silent:
+            print(line)
+
+    def error(self, unit: str, msg: str, silent: bool = True):
         now = datetime.datetime.now().time()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/ERROR]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
             file.write(line + "\n")
-        print(line)
+        if not silent:
+            print(line)
 
 
 logger = Logger()
