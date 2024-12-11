ad12338c8b519e0db80a64b5931a6cb1b368f8c4
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-12-11 08:44:42 +0200

    Fixes

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,11 +1,10 @@
 from .classes import Force, Position, Atom
 from .files import get_files, find_file_like, scandir
-from . import exceptions
 from . import distance
 from .logger import logger
 from .inputs import get_input
 from .visuals import graph
 
 __all__ = ["Force", "Position", "get_files",
-           "find_file_like", "scandir", "exceptions", "distance",
+           "find_file_like", "scandir", "distance",
            "Atom", "logger", "get_input", "graph"]
