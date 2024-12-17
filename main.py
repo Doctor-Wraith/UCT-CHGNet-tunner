6377c73fb57ce934b6d13c7624e40fbbed5a75ee
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-12-17 10:54:31 +0200

    Updated logging

--- main.py
+++ main.py
@@ -50,4 +50,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        logger.error("main", "CRASHING")
--- src/util/logger.py
+++ src/util/logger.py
@@ -5,7 +5,7 @@ class Logger:
     def __init__(self, save_path: str = "./logs.log") -> None:
         self.save_path = save_path
 
-    def info(self, unit: str, msg: str, silent: bool = True):
+    def info(self, unit: str, msg: str, silent: bool = False):
         now = datetime.datetime.now()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/INFO]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
@@ -13,7 +13,7 @@ class Logger:
         if not silent:
             print(line)
 
-    def warn(self, unit: str, msg: str, silent: bool = True):
+    def warn(self, unit: str, msg: str, silent: bool = False):
         now = datetime.datetime.now()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/WARN]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
@@ -21,7 +21,7 @@ class Logger:
         if not silent:
             print(line)
 
-    def error(self, unit: str, msg: str, silent: bool = True):
+    def error(self, unit: str, msg: str, silent: bool = False):
         now = datetime.datetime.now()
         line = f"{now.strftime('%d/%m/%Y %H:%M:%S')}-[{unit}/ERROR]:\t{msg}" # noqa
         with open(self.save_path, "a") as file:
