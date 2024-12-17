d527332efb546427d6867018708df2f42c6d85e3
Thomas van Steen <thomas.vansteen2004@gmail.com>
2024-12-17 07:50:50 +0200

    updated run files

--- run.ps1
+++ run.ps1
@@ -1,3 +1,6 @@
 git pull
+py -m pip install virtualenv
+py -m venv .venv
 .venv\Scripts\Activate.ps1
+pip install -r .\requirements.txt
 .\main.py
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 git pull
+python3 -m pip install virtualenv
+python3 -m venv .venv
 source ./.venv/bin/activate
-
+pip install -r ./requirements.txt
 python3 main.py
